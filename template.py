50b62c954ea32202742b9ee58573fb97940768b6
Dan McLaughlin <dan.mclaughlin@gmail.com>
2022-06-07 06:11:38 -0700

    Extensive cleanup and additions. More text explanatory documentation, and name checks and validations.

--- template.py
+++ template.py
@@ -16,6 +16,62 @@ def rename_unreal_asset(source_asset_path, destination_asset_path):
     return unreal.EditorAssetLibrary.rename_asset(source_asset_path, destination_asset_path)
 
 # -----------------------------------------------------------------------------
+# Utility function to check the validity of an XYZ name
+#
+
+def valid_XYZ_name(self):
+    """
+    Check whether is a valid XYZ formatted name
+    Returns multiple values
+         IsValid, asset name, subfolder, name split by "_", level, whether there is a "-" in the descriptive name
+    """
+    if self.extensions.XYZ.Logging == True:
+        print("   Before valid_XYZ_name")
+        
+    asset_data = self.asset_data[self.asset_id]
+    path, ext = asset_data['file_path'].split('.')
+    asset_path = asset_data['asset_path']
+    
+    ## TBD new plugin release which gives us the object name (James)
+    ## Sans that figure it out from the file path
+    ## splits = self.asset_data[self.asset_id]['asset_name'].split('_')
+    asset_name = os.path.split(asset_path)[1]   
+    asset_subfolder = asset_path.split('/')[-2]
+    splits = asset_name.split('_')
+   
+    
+    if len(splits) > 1: # Follows the XYZ naming 
+        level = splits[-1][0:3]
+        if level[0:1] == 'L' or level[0:1] == 'M':
+            if len(splits) == 4: # Special case of a "SM_Foo-Bar_LXXXXXX" type name
+                if self.extensions.XYZ.Logging == True:
+                    print("      Found XYZ dash formatted name: " + asset_name)
+                    print("   After valid_XYZ_name")
+                return True, asset_name, asset_subfolder, splits, level, True
+            else:
+                if self.extensions.XYZ.Logging == True:
+                    print("      Found XYZ formatted name: " + asset_name)
+                    print("   After valid_XYZ_name")
+                    return True, asset_name, asset_subfolder, splits, level, False
+        else:
+            if self.extensions.XYZ.Logging == True:
+                print("      A split up name but doesn't have the tag formatting: " + asset_name)
+                print("   After valid_XYZ_name")
+            return False, asset_name, asset_subfolder, splits, level, False
+    else:
+        if self.extensions.XYZ.Logging == True:
+            print("      Name has no _ seperators: " + asset_name)
+            print("   After valid_XYZ_name")
+            return False, asset_name, "", "", "", False    
+
+        
+    if self.extensions.XYZ.Logging == True:
+        print("   After valid_XYZ_name")
+        
+    return False, asset_name, asset_subfolder, splits, level, False
+           
+
+# -----------------------------------------------------------------------------
 # Utility function to get the XYZ name
 #
 def get_XYZ_name(self):
@@ -23,8 +79,24 @@ def get_XYZ_name(self):
     Only works during pre_asset_export/pre_animation_export I think
     Determine the name and Content path for an asset
     Returns a tuple path, asset_name
-    
-    XYZ format is 
+    A properly formatted name is of the form, e.g. 
+        
+        SM_Boss_L0123456789
+            or
+        SM_Boss-Manny_L0123456789
+        
+        Prefix_DescriptiveName-DescriptiveVariation_LABCDEFGHIJ
+        
+        The last split is the 'tag', which is a unique ID for the asset, prefixed by 'L' (Level) or 'M' (Map), the rest of the tag
+        indicates other information by two digit groups
+        L01 23 45 67 89 means
+           Level     01   # The level the asset first appears in
+           Scene     23   # The scene/location the asset first appears in
+           Lot       45   # The Lot, e.g. buildings = 01, characters = 02, etc, 
+           Asset     67   # Something specific to the asset
+           Variation 89   # An individual asset may have variations such as skins
+        
+    XYZ subdirectory format is
         Art/LXY/FullAssetName/FullAssetName                                For assets that follow the AB_Name_Tag format
         Art/LXY/FullAssetName/EnclosingFolder/FullAssetName/FullAssetName  For assets that follow the AB_Name_Tag format and are within a subdirectory
         Art/Misc/FullAssetName                                             For assets which have a prefix but no tag suffix
@@ -36,61 +108,57 @@ def get_XYZ_name(self):
 
     :param PropertyGroup self: The scene property group that contains all the addon properties.
     """
-    # pprint(self)
     pp = pprint.PrettyPrinter(indent=3)
     
     asset_data = self.asset_data[self.asset_id]
-    path, ext = asset_data['file_path'].split('.')
-    asset_path = asset_data['asset_path']
-    
+
     if self.extensions.XYZ.Logging == True:
         print("Before get_XYZ_name")
         
-    ## TBD new release
-    ## splits = self.asset_data[self.asset_id]['asset_name'].split('_')
-    asset_name = os.path.split(asset_path)[1]   
-    asset_subfolder = asset_path.split('/')[-2]
+    valid, asset_name, asset_subfolder, splits, level, variation = valid_XYZ_name(self)    
     
     if self.extensions.XYZ.Logging == True:
-        print("   Asset subfolder: " + asset_subfolder )
-        print("   Asset Name: " + asset_name )     
-    
-    splits = asset_name.split('_')
+        if asset_subfolder == None:
+            print("   No asset subfolder found")
+        else:
+            print("   Asset subfolder: " + asset_subfolder )
+        if asset_name == None:
+            print("   No asset_name found")
+        else: 
+            print("   Asset Name: " + asset_name )     
     
     if self.extensions.XYZ.Logging == True:
-        print("   Splits: ", end='')
-        pp.pprint(splits)
+        if splits == None:
+            print("   Name has no underscores (_)")
+        else:
+            print("   Splits: ", end='')
+            pp.pprint(splits)
         
     if self.extensions.XYZ.Type == 'DONE':
-        if len(splits) > 1: # Follows the XYZ naming 
-            level = splits[-1][0:3]
-            if level[0:1] == 'L' or level[0:1] == 'M':
-                if len(splits) == 4: # Special case of a "SM_Foo-Bar_LXXXXXX" type name
-                    if self.extensions.XYZ.Logging == True:
-                        print("      Found XYZ dash formatted name: " + asset_name)
-                    path = '/Game/Art/' + level + "/" + splits[0] + "_" + splits[1] + "_" + splits[3]
-                else:                # Regular case of "SM_Foo_LXXXXX"
-                    if self.extensions.XYZ.Logging == True:
-                        print("      Found XYZ formatted name: " + asset_name)
-                    path = '/Game/Art/' + level + "/" + asset_name 
-            else:
+        if valid == True:
+            if variation == True: # Variation type, put it in the same as the base 
                 if self.extensions.XYZ.Logging == True:
-                    print("      Found unleveled Misc asset: " + asset_name)
-                path = '/Game/Art/Misc'    
+                    print("      Found XYZ dash formatted Finished asset: " + asset_name)
+                path = '/Game/Art/' + level + "/" + splits[0] + "_" + splits[1] + "_" + splits[3]
+            else:                # Normal case of "SM_Foo_LXXXXX"
+                if self.extensions.XYZ.Logging == True:
+                    print("      Found XYZ formatted Finished asset: " + asset_name)
+                path = '/Game/Art/' + level + "/" + asset_name 
         else:
-            if self.extensions.XYZ.Logging == True:
-                print("      Attempted formatted but found unformatted Misc asset: " + asset_name)
-            path = '/Game/Art/Misc'
-            
+            # The name wasn't formatted correctly, but is supposed to be "Finished" - put it in Miscellanous
+            print("      Found improperly formatted asset name: " + asset_name)
+            print("      Asset goes into Misc")
+            path = '/Game/Misc/' + asset_name 
     elif self.extensions.XYZ.Type == 'MISC':
         if self.extensions.XYZ.Logging == True:
             print("      Found Misc asset: " + asset_name)
-        path = '/Game/Art/Misc'
-        
+        path = '/Game/Art/Misc' 
     elif self.extensions.XYZ.Type == 'TEST':
         if self.extensions.XYZ.Logging == True:
             print("      Found Test asset: " + asset_name)
         path = '/Game/Art/Test'
+    else:
+        print("Error - this should not occur")
     
     if asset_subfolder != "Export" and asset_subfolder != "":
         path = path + "/" + asset_subfolder   
@@ -183,13 +251,35 @@ class XYZExtension(ExtensionBase):
         if self.extensions.XYZ.Logging == True:
             print('Before pre_validations')
             
-        # Setting this to False will terminate execution in the validation phase
-        #if self.extensions.XYZ.hello_property != 'Hello world':
-        #    self.validations_passed = False
+        pass_accumulator = True
+        
+        pp = pprint.PrettyPrinter(indent=3)
+        if self.extensions.XYZ.Logging == True:
+            print("   Asset Data:")
+            pp.pprint(self.asset_data)
+        
+        # asset_data = self.asset_data[self.asset_id]
+        
+        # valid, asset_name, asset_subfolder, splits, level, variation = valid_XYZ_name(self)        
+
+        # TBD unfortunately we can't do asset validations here
+        # this seems to be for general validations - not sure what those are
+        # if self.extensions.XYZ.Type == 'DONE':
+            # if valid == True:
+                # if self.extensions.XYZ.Logging == True:
+                    # print("   Pass: Finished asset has proper name structure")
+            # else:
+                # print("   Fail: Finished asset doesn't have valid XYZ name structure")
+                # pp.pprint(asset_data)
+                # pass_accumulator = False
+
+        self.validations_passed = pass_accumulator
         
         if self.extensions.XYZ.Logging == True:
             print('After pre_validations')
             print()
+            
+
 
     # -----------------------------------------------------------------------------
     # Pre export of mesh types
@@ -210,9 +300,7 @@ class XYZExtension(ExtensionBase):
             print("   Initial asset_data: ")            
             pp.pprint(asset_data)
 
-            
-
-        
+             
         asset_data['asset_folder'] = path         
         asset_data['asset_path'] = path + "/" + asset_name 
         
@@ -246,11 +334,8 @@ class XYZExtension(ExtensionBase):
             print("   Initial asset_data: ")
             pp.pprint(asset_data)        
   
-        #path = path + "/" + "Animation" 
-        
-        #skeleton_asset_path = asset_data.get('skeleton_asset_path').replace('_Skeleton', '')
-        #self.asset_data[self.asset_id]['skeleton_asset_path'] = f'{skeleton_asset_path}_added_this_Skeleton'
-        
+        path = path + "/" + "Animation" 
+     
         asset_data['asset_folder'] = path  
         asset_data['skeleton_asset_path'] = path + "/" + asset_name 
         
